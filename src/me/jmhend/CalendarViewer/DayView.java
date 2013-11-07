25f92546b836e0b874da2c2be61f1391cd377af5
Jesse Hendrickson <jmhendrickson@gmail.com>
2013-11-07 16:34:33 -0500

    Adding DayView EventDrawable layouting

--- src/me/jmhend/CalendarViewer/DayView.java
+++ src/me/jmhend/CalendarViewer/DayView.java
@@ -1,13 +1,21 @@
 package me.jmhend.CalendarViewer;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
-import android.view.View.MeasureSpec;
+import android.widget.TextView;
 
 /**
  * A View displaying the Events of a day.
@@ -27,17 +35,29 @@ public class DayView extends View {
 //// Member variables.
 ////============================================================================
 	
+	private List<EventDrawable> mEvents;
+	private Map<EventDrawable, Rect> mRectMap = new HashMap<EventDrawable, Rect>();
+	
 	private int mWidth;
 	private int mHourWidth;
-	private int mHourTextSize;
 	private int mHourHeight;
 	private int mLineHeight;
 	private int mPaddingTop;
+	private int mEventTitleHeight;
+	private int mEventLocationHeight;
+	private int mEventXPadding;
+	
+	private int mHourTextSize;
+	private int mTitleTextSize;
+	private int mLocationTextSize;
 	
 	private Paint mLinePaint;
 	private Paint mHourPaint;
 	private Paint mEventPaint;
-	private Paint mEventTextPaint;
+	private Paint mEventTitlePaint;
+	private Paint mEventLocationPaint;
+	
+	private Calendar mCalendar;
 	
 	
 ////============================================================================
@@ -79,9 +99,15 @@ public class DayView extends View {
 	private void init() {
 		mPaddingTop = 80;
 		mHourWidth = 140;
-		mHourTextSize = 40;
 		mHourHeight = 200;
 		mLineHeight = 3;
+		mEventTitleHeight = 60;
+		mEventLocationHeight = 45;
+		mEventXPadding = 6;
+		
+		mHourTextSize = 40;
+		mTitleTextSize = 40;
+		mLocationTextSize = 30;
 		
 		mLinePaint = new Paint();
 		mLinePaint.setAntiAlias(true);
@@ -99,6 +125,16 @@ public class DayView extends View {
 		mEventPaint.setAntiAlias(true);
 		mEventPaint.setColor(0x660088CC);
 		mEventPaint.setStyle(Style.FILL);
+		
+		mEventTitlePaint = new Paint();
+		mEventTitlePaint.setAntiAlias(true);
+		mEventTitlePaint.setColor(0xCCFFFFFF);
+		mEventTitlePaint.setTextAlign(Paint.Align.LEFT);
+		mEventTitlePaint.setStyle(Style.FILL);
+		mEventTitlePaint.setTextSize(mTitleTextSize);
+		mEventTitlePaint.setTypeface(Typeface.DEFAULT_BOLD);
+		
+		mCalendar = Calendar.getInstance();
 	}
 	
 ////============================================================================
@@ -112,6 +148,7 @@ public class DayView extends View {
 	@Override
 	protected void onSizeChanged(int width, int height, int oldw, int oldh) {
 		mWidth = width;
+		calculateEventRects();
 	}
 	
 	/*
@@ -136,23 +173,7 @@ public class DayView extends View {
 	@Override
 	public void onDraw(Canvas canvas) {
 		drawHoursAndLines(canvas);
-		
-		mEventPaint.setColor(0x660088CC);
-		int top = 2 * mHourHeight + mPaddingTop;
-		int bottom = top + mHourHeight * 2;
-		canvas.drawRect(mHourWidth, top, mWidth, bottom, mEventPaint);
-		
-		int top2 = top + mHourHeight;
-		int bottom2 = bottom + mHourHeight / 2;
-		canvas.drawRect(mHourWidth, top2, mWidth, bottom2, mEventPaint);
-		
-		
-		int top3 = top2 - mHourHeight / 4;
-		int bottom3 = bottom2 + mHourHeight;
-		int width = mHourWidth + (mWidth - mHourWidth) / 2;
-		
-		mEventPaint.setColor(0x6622AA00);
-		canvas.drawRect(width, top3, mWidth, bottom3, mEventPaint);
+		drawEventRects(canvas);
 	}
 	
 	/**
@@ -167,8 +188,33 @@ public class DayView extends View {
 		}
 	}
 	
-	private void drawEvents(Canvas canvas) {
-		
+	/**
+	 * Draws each event Rect to the canvas.
+	 */
+	private void drawEventRects(Canvas canvas) {
+		if (mEvents == null) {
+			return;
+		}
+		for (EventDrawable event : mEvents) {
+			Rect rect = mRectMap.get(event);
+			if (rect != null) {
+				
+				// Draw transluscent background.
+				canvas.drawRect(rect, mEventPaint);
+				
+				// Draw border
+				Rect border = new Rect(rect);
+				border.right = border.left + 10;
+				canvas.drawRect(border, mEventPaint);
+				canvas.drawRect(border, mEventPaint);
+				
+				// Draw Title
+				canvas.drawText(event.getDrawableTitle(), rect.left + 20, rect.top + (mTitleTextSize) + 10, mEventTitlePaint);
+				// Draw Location.
+			} else {
+				Log.e(TAG, "Drawing null Rect");
+			}
+		}
 	}
 	
 	/**
@@ -187,5 +233,171 @@ public class DayView extends View {
 		}
 		return (position - 12) + "";
 	}
+	
+////============================================================================
+//// Event positioning.
+////============================================================================
+	
+	/**
+	 * @return The EventDrawables to draw on this DayView.
+	 */
+	private List<EventDrawable> getEvents() {
+		if (mEvents == null) {
+			mEvents = getTestEvents();
+		}
+		return mEvents;
+	}
+	
+	/**
+	 * Calculates the positiong of EventDrawables within this DayView.
+	 */
+	protected void calculateEventRects() {
+		mRectMap.clear();
+		List<EventDrawable> events = getEvents();
+		int i = 0;
+		int size = events.size();
+		int minY = -1;
+		
+		List<Rect> groupedRects = new ArrayList<Rect>();
+		
+		while (i < size) {
+			EventDrawable event = events.get(i);
+			if (!willDrawEvent(event)) {
+				continue;
+			}
+			
+			int topY = getYForTime(event.getDrawingStartTime());
+			int bottomY = getYForTime(event.getDrawingEndTime());
+			int textBottom = topY + getTextHeight(event);
+			
+			// minY hasn't been set yet.
+			if (minY == -1) {
+				minY = topY;
+			}
+			
+			// New collision group; layout previous group.
+			if (topY > minY) {
+				layoutGroupRects(groupedRects);
+			}
+			
+			Rect r = new Rect();
+			r.top = topY;
+			r.bottom = bottomY;
+			groupedRects.add(r);
+			mRectMap.put(event, r);
+			
+			// End of events.
+			if (i == (size - 1)) {
+				layoutGroupRects(groupedRects);
+			}
+			
+			minY = Math.max(minY, textBottom);
+			i++;
+		}
+	}
+	
+	/**
+	 * Lays out a collision group of event Rects.
+	 * @param groupedRects
+	 */
+	private void layoutGroupRects(List<Rect> groupedRects) {
+		int collisionCount = groupedRects.size();
+		int width = (mWidth - mHourWidth) / collisionCount;
+		
+		// Calculate positions for the collision group's events.
+		for (int j = 0; j < collisionCount; j++) {
+			int startX = mHourWidth + j * width;
+			int endX = startX + width - mEventXPadding;
+			Rect er = groupedRects.get(j);
+			er.left = startX;
+			er.right = endX;
+		}
+		groupedRects.clear();
+	}
+	
+	/**
+	 * @return The y position in this DayView that correlates to the 'time'.
+	 */
+	private int getYForTime(long time) {
+		mCalendar.setTimeInMillis(time);
+		int hourY = mCalendar.get(Calendar.HOUR_OF_DAY) * mHourHeight + mPaddingTop;
+		float offsetPercent = ((float) mCalendar.get(Calendar.MINUTE) / (float) 60f);
+		int minOffset = (int) (offsetPercent * mHourHeight);
+		return hourY + minOffset;
+	}
+	
+	/**
+ 	 * @return The height needed to display the EventDrawable's text.
+	 */
+	private int getTextHeight(EventDrawable e) {
+		if (e.getTextLinesCount() == 2) {
+			return mEventTitleHeight + mEventLocationHeight;
+		}
+		return mEventTitleHeight;
+	}
+	
+	/**
+	 * @return True if the EventDrawable will be drawn in this DayView, false otherwise.
+	 */
+	private boolean willDrawEvent(EventDrawable e) {
+		return true;
+	}
+	
+////============================================================================
+//// Test
+////============================================================================
 
+	private List<EventDrawable> getTestEvents() {
+		List<EventDrawable> events = new ArrayList<EventDrawable>();
+		events.add(new TestEvent(1383832800000L, 1383865200000L));
+		events.add(new TestEvent(1383840000000L, 1383865200000L));
+		events.add(new TestEvent(1383847200000L, 1383850800000L));
+		events.add(new TestEvent(1383847200000L, 1383854300000L));
+		events.add(new TestEvent(1383848100000L, 1383854300000L));
+		events.add(new TestEvent(1383854400000L, 1383863400000L));
+		return events;
+	}
+	
+	private static class TestEvent implements EventDrawable {
+		
+		private long start;
+		private long end;
+		
+		public TestEvent(long start, long end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		@Override
+		public long getDrawingStartTime() {
+			return start;
+		}
+		@Override
+		public long getDrawingEndTime() {
+			return end;
+		}
+		@Override
+		public boolean isDrawingAllDay() {
+			return false;
+		}
+		@Override
+		public int getDrawingColor() {
+			return 0;
+		}
+		@Override
+		public int getTextLinesCount() {
+			return 1;
+		}
+
+		@Override
+		public String getDrawableTitle() {
+			return "Howdy y'all";
+		}
+
+		@Override
+		public String getDrawingLocation() {
+			return "Copacabana";
+		}
+		
+	}
 }
---
+++ src/me/jmhend/CalendarViewer/EventDrawable.java
@@ -0,0 +1,46 @@
+package me.jmhend.CalendarViewer;
+
+/**
+ * Event model that can provides fields on how it should be drawn.
+ * @author jmhend
+ *
+ */
+public interface EventDrawable {
+	
+	/**
+	 * @return Start time in millis of the event.
+	 */
+	public long getDrawingStartTime();
+	
+	/**
+	 * @return End time in millis of the event.
+	 */
+	public long getDrawingEndTime();
+	
+	/**
+	 * @return True if the event is an all day event.
+	 */
+	public boolean isDrawingAllDay();
+	
+	/**
+	 * @return The color to draw for the event.
+	 */
+	public int getDrawingColor();
+	
+	/**
+	 * @return How many lines of text this event needs to draw.
+	 */
+	public int getTextLinesCount();
+	
+	/**
+	 * @return Title.
+	 */
+	public String getDrawableTitle();
+	
+	/**
+	 * @return Location
+	 */
+	public String getDrawingLocation();
+	
+
+}
